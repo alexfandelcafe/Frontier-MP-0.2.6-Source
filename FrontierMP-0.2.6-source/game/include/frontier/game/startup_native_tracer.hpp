71439d863964cf293b2403f4a1a96e555734ebd8
Alex Martinefsky <129073991+alexfandelcafe@users.noreply.github.com>
2026-09-22 20:25:55 -0300

    Add transition native tracer handlers

--- FrontierMP-0.2.6-source/game/include/frontier/game/startup_native_tracer.hpp
+++ FrontierMP-0.2.6-source/game/include/frontier/game/startup_native_tracer.hpp
@@ -17,6 +17,11 @@ private:
     static void script_done_loading_hook(void* context);
     static void clear_mission_info_hook(void* context);
     static void launch_new_script_hook(void* context);
+    static void launch_new_script_with_args_hook(void* context);
+    static void set_mission_info_hook(void* context);
+    static void was_last_reset_for_multiplayer_hook(void* context);
+    static void is_launch_retail_hook(void* context);
+    static void is_simulate_start_press_hook(void* context);
 
     static void log(const char* message);
 
@@ -25,6 +30,11 @@ private:
     NativeInvoker::NativeHandler originalScriptDoneLoading_{};
     NativeInvoker::NativeHandler originalClearMissionInfo_{};
     NativeInvoker::NativeHandler originalLaunchNewScript_{};
+    NativeInvoker::NativeHandler originalLaunchNewScriptWithArgs_{};
+    NativeInvoker::NativeHandler originalSetMissionInfo_{};
+    NativeInvoker::NativeHandler originalWasLastResetForMultiplayer_{};
+    NativeInvoker::NativeHandler originalIsLaunchRetail_{};
+    NativeInvoker::NativeHandler originalIsSimulateStartPress_{};
     bool attached_{};
 };
 
