f757f0739c4dff9d97c117398330ceb55b029dc9
Alex Martinefsky <129073991+alexfandelcafe@users.noreply.github.com>
2026-09-23 04:43:53 -0300

    Add passive actor-slot trace hooks

--- FrontierMP-0.2.6-source/game/include/frontier/game/startup_native_tracer.hpp
+++ FrontierMP-0.2.6-source/game/include/frontier/game/startup_native_tracer.hpp
@@ -35,6 +35,10 @@ private:
     static void create_player_actor_in_layout_hook(void* context);
     static void get_player_actor_hook(void* context);
     static void get_actor_enum_hook(void* context);
+    static void get_actor_slot_hook(void* context);
+    static void get_slot_actor_hook(void* context);
+    static void get_local_slot_hook(void* context);
+    static void is_slot_valid_hook(void* context);
     static void create_actor_in_layout_hook(void* context);
     static void create_layout_hook(void* context);
     static void find_named_layout_hook(void* context);
@@ -65,6 +69,10 @@ private:
     NativeInvoker::NativeHandler originalCreatePlayerActorInLayout_{};
     NativeInvoker::NativeHandler originalGetPlayerActor_{};
     NativeInvoker::NativeHandler originalGetActorEnum_{};
+    NativeInvoker::NativeHandler originalGetActorSlot_{};
+    NativeInvoker::NativeHandler originalGetSlotActor_{};
+    NativeInvoker::NativeHandler originalGetLocalSlot_{};
+    NativeInvoker::NativeHandler originalIsSlotValid_{};
     NativeInvoker::NativeHandler originalCreateActorInLayout_{};
     NativeInvoker::NativeHandler originalCreateLayout_{};
     NativeInvoker::NativeHandler originalFindNamedLayout_{};
