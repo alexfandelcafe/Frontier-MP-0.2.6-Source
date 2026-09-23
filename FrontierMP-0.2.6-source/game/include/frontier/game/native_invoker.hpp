972f5cb42befbb624e058bd488997c4c3b86b5b0
Alex Martinefsky <129073991+alexfandelcafe@users.noreply.github.com>
2026-09-23 06:28:02 -0300

    Add mutable native invocation for output arguments

--- FrontierMP-0.2.6-source/game/include/frontier/game/native_invoker.hpp
+++ FrontierMP-0.2.6-source/game/include/frontier/game/native_invoker.hpp
@@ -19,6 +19,8 @@ public:
     bool invoke_u32(std::uint32_t hash, std::uint32_t& out) const;
     bool invoke_raw(std::uint32_t hash, const std::uintptr_t* arguments,
                     std::size_t argumentCount, std::uintptr_t& out) const;
+    bool invoke_raw_mutable(std::uint32_t hash, std::uintptr_t* arguments,
+                             std::size_t argumentCount, std::uintptr_t& out) const;
     bool invoke_bool(std::uint32_t hash, bool& out) const;
 
     bool hook_native(std::uint32_t hash, NativeHandler replacement, NativeHandler& original, std::string* error = nullptr);
