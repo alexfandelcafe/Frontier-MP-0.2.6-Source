2338976b581c16b4308b71a948b7565ff2e17adb
Alex Martinefsky <129073991+alexfandelcafe@users.noreply.github.com>
2026-09-26 07:17:20 -0300

    Route CEF lifecycle through the RDR game thread

--- FrontierMP-0.2.6-source/client/include/frontier/client/cef_overlay.hpp
+++ FrontierMP-0.2.6-source/client/include/frontier/client/cef_overlay.hpp
@@ -3,6 +3,10 @@
 #include <atomic>
 #include <thread>
 
+namespace frontier::game {
+class RdrBridge;
+}
+
 namespace frontier::client {
 
 class CefOverlay final {
@@ -13,14 +17,18 @@ public:
     CefOverlay(const CefOverlay&) = delete;
     CefOverlay& operator=(const CefOverlay&) = delete;
 
-    bool start();
+    bool start(frontier::game::RdrBridge& bridge);
     void stop();
 
 private:
     void thread_main();
+    bool initialize_on_game_thread();
+    void pump_on_game_thread();
+    void shutdown_on_game_thread();
 
     std::atomic<bool> stopRequested_{false};
     std::thread thread_{};
+    frontier::game::RdrBridge* bridge_{};
 };
 
 } // namespace frontier::client
